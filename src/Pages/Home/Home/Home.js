5500235a2caa5f0a4460ba59926c144935bb90fb
itzrizvi <shahriar.me.work@gmail.com>
2021-11-10 18:28:48 +0600

    Banner Done

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -2,11 +2,14 @@ import React from 'react';
 import Footer from '../../Shared/Footer/Footer';
 import Navigation from '../../Shared/Navigation/Navigation';
 
+import Banner from '../Banner/Banner';
+
 
 const Home = () => {
     return (
         <>
             <Navigation />
+            <Banner />
 
             <Footer />
         </>
